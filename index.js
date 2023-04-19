ce5b465411d1e78c9c496f54d4d30e6d71fb3d20
Kelvin Tsang <kelvince@connect.hku.hk>
2023-04-20 00:44:42 +0100

    Compare source and target images and return an array of matching faces (#1)
    * Rename var to buffer (type)
    
    * Create collection, index faces, and search faces command functions in class rekog
    
    * Remove compare faces function
    
    * Remove compare faces function
    
    * Return matching images
    
    ---------
    
    Co-authored-by: Kelvin <kelvin@Kelvins-M1-Pro.local>

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable max-len */
 const downloader = require('./lib/downloader');
 const REKOG = require('./lib/aws/rekognition');
 
@@ -5,11 +7,48 @@ const config = require('./config');
 
 const rekog = new REKOG(config.region);
 
+async function createRekogCollection(collectionId) {
+  try {
+    await rekog.createCollection(collectionId);
+  } catch (err) {
+    if (err.message.match(/already exists/)) {
+      console.log(`Skipping creation of collection: ${collectionId} as it already exists`);
+      return;
+    }
+    throw err;
+  }
+}
+
+async function importTargetImagesToCollection(imageNames, collectionId) {
+  const imageIds = [];
+  for await (const imageName of imageNames) {
+    const imageBuffer = await downloader.targetImage(imageName);
+    const imageId = await rekog.importImageToCollection(collectionId, imageBuffer);
+    imageIds.push(imageId);
+  }
+
+  return imageIds;
+}
+
 async function main() {
-  const sourceBuffer = await downloader.sourceImage('elon.jpeg');
-  const targetBuffer = await downloader.targetImage('elon_fam.jpeg');
-  const response = await rekog.compareFaces(sourceBuffer, targetBuffer);
-  console.log(JSON.stringify(response));
+  // create collection for all images
+  const collectionId = 'celebrities';
+  await createRekogCollection(collectionId);
+
+  const elonImageBuffer = await downloader.sourceImage('elon.jpeg');
+  const elonIndexes = await rekog.indexFace(collectionId, elonImageBuffer); // add elon musk face to collection
+
+  const targetImageList = ['elon_fam.jpeg', 'elon2.jpeg', 'bill_gates.jpeg', 'steve_jobs.jpeg'];
+  const imageIds = await importTargetImagesToCollection(targetImageList, collectionId); // return a list of imageIds after importing target images
+
+  const matchingImageIds = await rekog.searchImagesWithMatchedFace(collectionId, elonIndexes.faceId);
+  const matchingImages = [];
+  matchingImageIds.forEach((matchingImageId) => {
+    const index = imageIds.findIndex((element) => element === matchingImageId);
+    matchingImages.push(targetImageList[index]);
+  });
+
+  console.log(matchingImages);
 }
 
 main();
