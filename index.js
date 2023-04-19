d748a8d1bbe1c5a35b72010433cbb0883e45241d
Kelvin <kelvin@Kelvins-M1-Pro.local>
2023-04-20 00:10:49 +0100

    Remove compare faces function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable max-len */
 const downloader = require('./lib/downloader');
 const REKOG = require('./lib/aws/rekognition');
 
@@ -5,10 +7,46 @@ const config = require('./config');
 
 const rekog = new REKOG(config.region);
 
+async function createRekogCollection(collectionId) {
+  try {
+    await rekog.createCollection(collectionId);
+  } catch (err) {
+    if (err.message.match(/already exists/)) {
+      console.log(`Skipping creation of collection: ${collectionId} as it already exists`);
+      return;
+    }
+    throw err;
+  }
+}
+
+async function importTargetImagesToCollection(imageNames, collectionId) {
+  const imageIds = [];
+  for await (const imageName of imageNames) {
+    const imageBuffer = await downloader.targetImage(imageName);
+    const imageId = await rekog.importImageToCollection(collectionId, imageBuffer);
+    imageIds.push(imageId);
+  }
+
+  return imageIds;
+}
+
 async function main() {
-  const sourceBuffer = await downloader.sourceImage('elon.jpeg');
-  const targetBuffer = await downloader.targetImage('elon_fam.jpeg');
-  const response = await rekog.compareFaces(sourceBuffer, targetBuffer);
+  // create collection for all images
+  const collectionId = 'celebrities';
+  await createRekogCollection(collectionId);
+
+  const elonImageBuffer = await downloader.sourceImage('elon.jpeg');
+  const elonIndexes = await rekog.indexFace(collectionId, elonImageBuffer); // add elon musk face to collection
+
+  const targetImageList = ['elon_fam.jpeg'];
+  const imageIds = await importTargetImagesToCollection(targetImageList, collectionId);
+
+  // const source = await rekog.listFaces(sourceCollectionId);
+  // console.log(source);
+  // const target = await rekog.listFaces(targetCollectionId);
+  // console.log(target);
+
+  const response = await rekog.searchFaces(collectionId, elonIndexes.faceId);
   console.log(JSON.stringify(response));
 }
 
